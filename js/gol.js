9896c3ae1f79c6c4dedce5b2c23a9259f1b2cb68
BlueVajra <cory.leistikow@gmail.com>
2014-07-09 23:32:58 -0600

    user can toggle cells for the starting state of the grid

--- js/gol.js
+++ js/gol.js
@@ -1,12 +1,17 @@
 window.GOL = {
 
   initialize: function () {
-    this.createBoard()
+    this.createBoard();
+    $(document).on("click", "#start_button", this.startGame.bind(this));
 
   },
 
   createBoard: function () {
     Board.initialize();
+  },
+
+  startGame: function () {
+    alert("starting the game");
   }
 
 }
@@ -16,20 +21,18 @@ window.Board = {
   initialize: function () {
     this.width = 10;
     this.height = 10;
-    this.grid = []
+    this.grid = [];
     this.createGrid();
     this.addGrid();
-
-
+    $(document).on("click", "#game_board div", this.toggleCell.bind(this));
   },
 
   createGrid: function () {
     for (var x = 0; x < this.height; x++) {
-      var new_column = []
+      var new_column = [];
       for (var y = 0; y < this.width; y++) {
         var new_node = new Cell();
         new_node.initialize(x, y);
-//        new_column.push(new Node.initialize(x,y));
         new_column.push(new_node)
       }
       this.grid.push(new_column);
@@ -37,15 +40,27 @@ window.Board = {
   },
 
   addGrid: function () {
-    for (var x = 0; x < this.height; x++) {
-      for (var y = 0; y < this.width; y++) {
-        $html = this.grid[x][y].$element
-        $('#game_board').append($html)
+    for (var y = 0; y < this.height; y++) {
+      for (var x = 0; x < this.width; x++) {
+        $html = this.grid[x][y].$element;
+        $('#game_board').append($($html));
       }
       $('#game_board').append('<br />');
     }
+  },
+
+  toggleCell: function (event) {
+    $thisCell = $(event.target)
+    if ($thisCell.data().alive === "true") {
+      $thisCell.data("alive", "false");
+      $thisCell.removeClass('alive');
+    } else {
+      $thisCell.data("alive", "true");
+      $thisCell.addClass('alive');
+
+    }
   }
-}
+};
 
 var Cell;
 Cell = function Cell() {
@@ -60,9 +75,12 @@ Cell = function Cell() {
   };
 
   Cell.prototype.node_div = function () {
-    this.$element = "<div id='node-" + this.x + "-" + this.y + "'></div>"
+    this.$element = "<div data-x='" + this.x + "' data-y='" + this.y + "' data-alive='false' class='node' id='node-" + this.x + "-" + this.y + "'></div>";
   };
 
+  Cell.prototype.showMe = function () {
+    console.log(this)
+    alert("hey there")
+  };
 
-}
-
+};
