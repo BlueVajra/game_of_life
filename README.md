49574af28600c8acd6eed0d51bcdd872b889162c
BlueVajra <cory.leistikow@gmail.com>
2014-07-11 16:27:17 -0600

    User can set initial state and start-stop the game
    Implemented basic logic with debugging statements in place

---
+++ README.md
@@ -0,0 +1,31 @@
+# Cory's 'Conway's Game of Life'
+
+This was my first attempt at recreating the classic Conway's Game of Life. I won't go into details here, but you can find more information at http://en.wikipedia.org/wiki/Conway's_Game_of_Life
+
+This was written in JavaScript (with the help of jQuery). While not a difficult project by any means, it is a classic code sample that I hope to improve over time. It has been used by many people to hone their code-craft, explore refactorings and OOP principles. The aim of this project is to do the same.
+
+So please, look through the commits, give me some feedback, let's improve our understanding of programming.
+
+Thanks!
+
+## wanna give it a try?
+
+1. Right now, just clone this repo and run it on your local browser... 
+1. click the squares on the grid to create an initial state, and press start!
+1. to get you started... here are a couple of fun shapes to get you started!
+
+Blinker oscillator
+```
+  x
+  x
+  x
+```
+
+Glider (watch it glide across the screen
+```
+ x
+  x
+xxx
+```
+
+Have fun!
--- js/gol.js
+++ js/gol.js
@@ -2,7 +2,9 @@ window.GOL = {
 
   initialize: function () {
     this.createBoard();
+    this.testing = false
     $(document).on("click", "#start_button", this.startGame.bind(this));
+    $(document).on("click", "#stop_button", this.stopGame.bind(this));
 
   },
 
@@ -11,7 +13,11 @@ window.GOL = {
   },
 
   startGame: function () {
-    alert("starting the game");
+    Board.startTimer();
+  },
+
+  stopGame: function () {
+    Board.stopTimer();
   }
 
 }
@@ -19,12 +25,14 @@ window.GOL = {
 window.Board = {
 
   initialize: function () {
-    this.width = 10;
-    this.height = 10;
+    this.width = 50;
+    this.height = 50;
     this.grid = [];
     this.createGrid();
     this.addGrid();
-    $(document).on("click", "#game_board div", this.toggleCell.bind(this));
+    this.timer = null;
+    this.lifeGrid = null;
+    $(document).on("click", "#game_board div", this.clickedDiv.bind(this));
   },
 
   createGrid: function () {
@@ -32,7 +40,7 @@ window.Board = {
       var new_column = [];
       for (var y = 0; y < this.width; y++) {
         var new_node = new Cell();
-        new_node.initialize(x, y);
+        new_node.initialize(x,y);
         new_column.push(new_node)
       }
       this.grid.push(new_column);
@@ -49,16 +57,98 @@ window.Board = {
     }
   },
 
-  toggleCell: function (event) {
-    $thisCell = $(event.target)
-    if ($thisCell.data().alive === "true") {
-      $thisCell.data("alive", "false");
+  clickedDiv: function (event) {
+    $thisCell = $(event.target);
+    this.toggleCell($thisCell);
+  },
+
+  changeCell: function(x,y){
+    $thisCell = $("#node-"+ x +"-"+ y);
+    this.toggleCell($thisCell);
+  },
+
+  toggleCell: function (currentCell) {
+    $thisCell = currentCell
+    a = $thisCell.data().x;
+    b = $thisCell.data().y;
+
+    this.grid[a][b].toggle();
+
+    if ($thisCell.data().alive === true) {
+      $thisCell.data("alive", false);
       $thisCell.removeClass('alive');
     } else {
-      $thisCell.data("alive", "true");
+      $thisCell.data("alive", true);
       $thisCell.addClass('alive');
-
     }
+    if (GOL.testing) console.log($thisCell.data())
+  },
+
+  startTimer: function () {
+    this.timer = setInterval(function () {
+      Board.Tick();
+    }, 200);
+  },
+
+  stopTimer: function () {
+    clearInterval(this.timer);
+  },
+
+  Tick: function () {
+    this.checkLife();
+    this.nextLife();
+  },
+
+  checkLife: function () {
+    this.lifeGrid = []
+
+    for (var y = 0; y < this.height; y++) {
+      var new_row = [];
+      for (var x = 0; x < this.width; x++) {
+        new_row.push(this.grid[x][y].alive)
+      }
+      this.lifeGrid.push(new_row);
+    }
+
+    if (GOL.testing) console.log("---" + this.lifeGrid + "---")
+  },
+
+  nextLife: function () {
+    for (var y = 0; y < this.height; y++) {
+      for (var x = 0; x < this.width; x++) {
+        this.grid[x][y].nextLife();
+      }
+    }
+  },
+
+  neighborLife: function (x, y) {
+    var neighbors = [
+      [x - 1, y],
+      [x + 1, y],
+      [x - 1, y - 1],
+      [x, y - 1],
+      [x + 1, y - 1],
+      [x - 1, y + 1],
+      [x, y + 1],
+      [x + 1, y + 1]
+    ]
+
+    var aliveInfo = []
+    var alive = 0
+    for (var i = 0; i < 8; i++) {
+      if ((neighbors[i][0] >= 0 && neighbors[i][0] < this.width) && (neighbors[i][1] >= 0 && neighbors[i][1] < this.height)) {
+        if (this.lifeGrid[neighbors[i][1]][neighbors[i][0]]) {
+          alive++
+          aliveInfo.push(neighbors[i][1] + "-" + neighbors[i][0] + ":isAlive ")
+        }else{
+          aliveInfo.push(neighbors[i][1] + "-" + neighbors[i][0] + ":isDead ")
+        }
+      }
+    }
+
+    if (GOL.testing) console.log(x + "-" + y + ": " + aliveInfo.join(" | "))
+
+    return alive
   }
 };
 
@@ -71,16 +161,47 @@ Cell = function Cell() {
     this.y = y;
     this.$element = null;
     this.node_div();
-
+    this.id = "#node-" + this.x + "-" + this.y;
+    this.node = $(document).find($(this.id[0]))
   };
 
   Cell.prototype.node_div = function () {
-    this.$element = "<div data-x='" + this.x + "' data-y='" + this.y + "' data-alive='false' class='node' id='node-" + this.x + "-" + this.y + "'></div>";
+    this.$element = "<div data-alive='false' data-x='" + this.x + "' data-y='" + this.y + "' class='node' id='node-" + this.x + "-" + this.y + "'></div>";
   };
 
-  Cell.prototype.showMe = function () {
-    console.log(this)
-    alert("hey there")
+  Cell.prototype.toggle = function () {
+
+    if (this.alive === true) {
+      this.alive = false;
+    } else {
+      this.alive = true;
+    }
+    if (GOL.testing) console.log(this.x + "-" + this.y)
   };
 
+  Cell.prototype.nextLife = function () {
+    aliveNeighbors = Board.neighborLife(this.x, this.y);
+    testString = this.x + "-" + this.y + " "
+
+    if(this.alive){
+      if(aliveNeighbors < 2){
+        testString += "I'm Alive with " + aliveNeighbors + " live neighbors: I will die - CHANGE"
+        Board.changeCell(this.x,this.y)
+      }else if(aliveNeighbors === 2 || aliveNeighbors === 3){
+        testString += "I'm Alive with " + aliveNeighbors + " live neighbors: I will stay alive"
+      }else{
+        testString += "I'm Alive with " + aliveNeighbors + " live neighbors: I will die - CHANGE"
+        Board.changeCell(this.x,this.y)
+      }
+    }else{
+      if(aliveNeighbors === 3){
+        testString += "I'm Dead with " + aliveNeighbors + " live neighbors: I will come alive - CHANGE"
+        Board.changeCell(this.x,this.y)
+      }else{
+        testString += "I'm Dead with " + aliveNeighbors + " live neighbors: I will stay dead"
+      }
+    }
+    if (GOL.testing) console.log(testString);
+
+  };
 };
